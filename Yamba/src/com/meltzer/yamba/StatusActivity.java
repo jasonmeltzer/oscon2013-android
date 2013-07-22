7d722c7dde49e65283862babe7a125586b72dc72
Jason Meltzer <jmeltzer@godaddy.com>
2013-07-22 11:05:52 -0700

    Implemented event handling for button

--- Yamba/src/com/meltzer/yamba/StatusActivity.java
+++ Yamba/src/com/meltzer/yamba/StatusActivity.java
@@ -2,7 +2,10 @@ package com.meltzer.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class StatusActivity extends Activity {
 	
@@ -13,6 +16,20 @@ public class StatusActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+        
+        editStatus = (EditText) findViewById(R.id.edit_status);
+        buttonUpdate = (Button) findViewById(R.id.button_update);
+        buttonUpdate.setOnClickListener( new OnClickListener() {
+
+			@Override
+			/** 
+			 * What we want to do when the "update" button is clicked
+			 */
+			public void onClick(View view) {
+				Log.d("StatusActivity", "onClicked!");
+				
+			} 
+		});
     }
 
 
