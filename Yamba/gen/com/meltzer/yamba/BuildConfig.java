5f372dca3ecdf6bc54fa591ec10a4af5c770994e
Jason Meltzer <jmeltzer@godaddy.com>
2013-07-22 10:47:27 -0700

    adding Yamba project from first half of tutorial

---
+++ Yamba/gen/com/meltzer/yamba/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.meltzer.yamba;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Yamba/src/com/meltzer/yamba/StatusActivity.java
@@ -0,0 +1,16 @@
+package com.meltzer.yamba;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class StatusActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+    }
+
+
+}
